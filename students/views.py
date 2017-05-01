c61df651e4dbd39d7e39b11367c62a3e43857581
sanyache <sanyache@mail.ru>
2017-05-01 20:00:56 +0300

    ch 6

--- students/views.py
+++ students/views.py
@@ -5,7 +5,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def students_list(request):
-    return render(request,'students/students_list.html',{})
+    students=(
+        {'id': 1,
+         'first_name': u'Олександр',
+         'last_name' : u'Чемерис',
+         'ticket': 235,
+	 'image': 'img/che1.jpg'},
+	{'id' : 2,
+	 'first_name': u'Ірина',
+	 'last_name': u'Чемерис',
+	 'ticket': 236,
+	 'image': 'img/che2.jpg'},
+	{'id': 3,
+	 'first_name': u'Олександр',
+	 'last_name': u'Іваненко',
+	 'ticket': 237,
+	 'image': 'img/ivan.jpg'},
+    )
+
+    return render(request,'students/students_list.html',{'students': students})
 def students_edit(request,sid):
     return HttpResponse('<h1>Edit students %s</h1>'% sid)
 def students_add(request):
@@ -19,4 +37,24 @@ def groups_edit(request, gid):
 def groups_delete(request,gid):
     return HttpResponse('<h1>Delete Group %s</h1>'% gid)
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request,'students/groups_list.html',{})
+def journal(request):
+    students = (
+        {'id': 1,
+         'first_name': u'Олександр',
+         'last_name': u'Чемерис',
+         'ticket': 235,
+         'image': 'img/che1.jpg'},
+        {'id': 2,
+         'first_name': u'Ірина',
+         'last_name': u'Чемерис',
+         'ticket': 236,
+         'image': 'img/che2.jpg'},
+        {'id': 3,
+         'first_name': u'Олександр',
+         'last_name': u'Іваненко',
+         'ticket': 237,
+         'image': 'img/ivan.jpg'},
+    )
+
+    return render(request,'students/journal.html',{'range':range(30),'students':students})
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '-1h6=v&gy+kpl_-)-dmwyvy4cg-1mly^l345n%%*x41=t$+2k%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+PORTAL_URL='http://localhost:8000'
 ALLOWED_HOSTS = []
 
 
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentsdb.context_processors.students_proc'
             ],
         },
     },
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+from django.conf import global_settings
+
